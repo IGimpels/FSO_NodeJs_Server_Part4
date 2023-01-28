1926404f2146b0fe051085071dd8e1b7e0426631
IGimpels <ilia.gimpels@thruinc.com>
2023-01-28 20:13:00 +0400

    4.6*: helper functions and unit tests, step4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = () => {
   return 1
 }
@@ -12,8 +14,18 @@ const favoriteBlog = (blogs) => {
   return blogs.length === 0 ? null : blogs.find(b => b.likes === Math.max(...blogs.map(b => b.likes)))
 }
 
+const mostBlogs = (blogs) => {
+  return _(blogs).groupBy('author').map((v,k) => {
+    return {
+      author: k,
+      blogs: v.length
+    }
+  }).maxBy('blogs')
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
